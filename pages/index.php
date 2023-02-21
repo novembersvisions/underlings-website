e91e1e2019aec29c12361576543dceceacab0b46
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-02-20 21:28:02 -0500

    header partial

--- pages/index.php
+++ pages/index.php
@@ -4,13 +4,16 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" type="image/x-icon" href="public/images/wug.png">
+  <link rel="stylesheet" href="public/styles/style.css" />
 
-  <title>Home - Underlings</title>
-  <link rel="icon" type="image/x-icon" href="/images/wug.png">
+  <title>Cornell Underlings</title>
 </head>
 
 <body>
-  TODO
+
+<?php include 'partials/header.php'; ?>
+
 </body>
 
 </html>
---
+++ partials/header.php
@@ -0,0 +1,20 @@
+<div class="header">
+    <a href="/">
+        <img src="public/images/wug.png" width="58" height="58">
+        <h1>Cornell Underlings</h1>
+    </a>
+
+    <nav>
+        <ul class="navbar">
+            <h3>
+
+            <li class="nav"><a href="/about" class="<?php echo $nav_about; ?>">About</a></li>
+            <li class="nav"><a href="/culc" class="<?php echo $nav_culc; ?>">CULC</a></li>
+            <li class="nav"><a href="/events" class="<?php echo $nav_events; ?>">Events</a></li>
+            <li class="nav"><a href="/contact" class="<?php echo $nav_contact; ?>">Contact</a></li>
+
+            </h3>
+        </ul>
+    </nav>
+
+</div>
