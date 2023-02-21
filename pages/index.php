85f22911a682726bf7a121473a897ca326386878
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-02-21 12:20:48 -0500

    sections on homepage

--- pages/index.php
+++ pages/index.php
@@ -38,6 +38,14 @@ $nav_contact = '';
   <a href="https://cornell.campusgroups.com/UnderLings/club_signup" target="_blank"><div class="home-2-button"><p>Join Us</p></div></a>
 </div>
 
+<div class="home-3">
+  <div class="home-3-text">
+    <h1>CULC 17</h1>
+    <h2>Cornell University's undergraduate linguistics association presents its 17th annual undergraduate research colloquium. By facilitating communication and discussion between researchers, the conference aims to promote undergraduate research at all levels throughout the linguistics community.</h2>
+    <a href="/culc"><div class="home-3-button"><p>Register</p></div></a>
+  </div>
+</div>
+
 </body>
 
 </html>
