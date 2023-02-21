e150dfa2b7da8b5e0399ca95d6630a2f7a69e6ba
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-02-21 13:09:49 -0500

    about page

--- pages/about.php
+++ pages/about.php
@@ -21,6 +21,31 @@ $nav_contact = '';
 
   <?php include 'partials/header.php'; ?>
 
+    <div class="about-1-text">
+        <h1>About Us</h1>
+
+        <h2>Cornell Underlings is the undergraduate student group of the Cornell Department of Linguistics.</h2>
+        <h2>It serves to seek unity in the body of linguistics majors and promote linguistics at the undergraduate level at Cornell as well as the broader academic community.</h2>
+        <h2>The Cornell Underlings invites anyone who is interested in languages, language patterns, language usage, language change, or anything language-related to join us in Morrill Hall. You don't have to be in the linguistics major to experience be a part of the Underlings!</h2>
+        <h2>We have weekly meetings in which members plan events and activities, discuss linguistics and share related information, and simply have fun chatting about classes and professors.</h2>
+        <a href="https://cornell.campusgroups.com/UnderLings/club_signup" target="_blank"><div class="about-button"><p>Join Us</p></div></a>
+    </div>
+
+    <div class="about-2-text">
+        <h1>Officers</h1>
+
+        <img src="public/images/wug.png" width="100">
+        <img src="public/images/wug.png" width="100">
+        <img src="public/images/wug.png" width="100">
+        <img src="public/images/wug.png" width="100">
+        <ul class="officers">
+            <li>President<br> Charlie Preston</li>
+            <li>Vice President<br> Luis Hernandez</li>
+            <li>Secretary<br> Jaden Cumpston</li>
+        </ul>
+
+    </div>
+
 </div>
 
 </body>
--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <p>Cornell Underlings &copy; 2023 | <a href="https://linguistics.cornell.edu/undergraduate-program">Cornell Linguistics Department</a></p>
+    <p>Cornell Underlings &copy; 2023 | <a href="https://linguistics.cornell.edu/undergraduate-program" target="_blank">Cornell Linguistics Department</a></p>
 </div>
